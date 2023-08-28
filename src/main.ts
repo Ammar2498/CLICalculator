bdca89d552650699f9822fc64544e2bed70b750c
Ammar kamal <141824801+Ammar2498@users.noreply.github.com>
2023-08-28 17:21:39 +0500

    version 1.1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,49 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 import { getAnswer } from "./calc.js";
-
-
-
-
+import { UserRegisteration } from "../login.js";
+import { userSignIn } from "../login.js";
 
 async function main() {
+  let SignUp = await inquirer.prompt([
+    {
+      message: "Create Your User name",
+      type: "input",
+      name: "userName",
+    },
+    {
+      message: "Create your Password",
+      type: "password",
+      name: "userPassword",
+    },
+  ]);
+
+  UserRegisteration(SignUp.userName, SignUp.userPassword);
+  console.log("User Has been registered");
+
+  let signIn = await inquirer.prompt([
+    {
+      message: "Enter your User Name",
+      type: "input",
+      name: "userName",
+    },
+    {
+      message: "Enter Your Password ",
+      type: "password",
+      name: "userPassword",
+    },
+  ]);
+
+if (userSignIn(signIn.userName,signIn.userPassword)){
+
+  console.log("Login Sucessfull");
+  
+
+
   let ans = await inquirer.prompt([
     {
-        
-      message:"Enter Your First Number ",
-      
+      message: "Enter Your First Number ",
+
       type: "number",
       name: "Num1",
     },
@@ -30,9 +62,11 @@ async function main() {
   ]);
 
   let result = getAnswer(ans.Num1, ans.Num2, ans.operator);
-  const formated = chalk.bold.green(`Result ${result}`)
+  const formated = chalk.bold.green(`Result ${result}`);
   console.log(formated);
-  
-
+}
+else {
+  console.log("Login Failed")
+}
 }
 main();
